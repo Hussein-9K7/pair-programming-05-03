48f915627b9b066d14536819562fb8650ae5e42c
faripanah <farnaz.jamilpanah@metropolia.fi>
2025-03-06 09:51:43 +0200

    updated productListing

--- frontend/src/components/ProductListing.jsx
+++ frontend/src/components/ProductListing.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 const ProductListing = ({product}) => {
     return (
       <div className="job-preview">
@@ -5,6 +6,9 @@ const ProductListing = ({product}) => {
         <p>Type: {product.category}</p>
         <p>Description: {product.description}</p>
         <p>Supplier:{product.supplier}</p>
+        <p>Rating:{product.rating}</p>
+        <Link to={`/products/${product.id}`}  className="btn">View Product</Link> 
+        <Link to={`/edit-product/${product.id}`} className="btn">Edit Product</Link>
       </div>
     );
   };
